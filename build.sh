cb24719361813d488f8e783f3cd9e022ecf1afb5
RyanJeong <wat6988@gmail.com>
2022-11-04 15:46:56 +0900

    fix: Fixed script to be able to compile 1.0.26

--- build.sh
+++ build.sh
@@ -5,14 +5,12 @@ WORKING_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"
 
 # alsa-lib
 ALSA_LIB_VERSION_MAJOR=1
-ALSA_LIB_VERSION_MINOR=2
-ALSA_LIB_VERSION_PATCH1=7
-ALSA_LIB_VERSION_PATCH2=2
+ALSA_LIB_VERSION_MINOR=0
+ALSA_LIB_VERSION_PATCH=26
 ALSA_LIB="alsa-lib-\
 $ALSA_LIB_VERSION_MAJOR"."\
 $ALSA_LIB_VERSION_MINOR"."\
-$ALSA_LIB_VERSION_PATCH1"."\
-$ALSA_LIB_VERSION_PATCH2"
+$ALSA_LIB_VERSION_PATCH"
 ALSA_LIB_FILENAME=$WORKING_DIR/$ALSA_LIB.tar.bz2
 ALSA_LIB_FOLDER=$WORKING_DIR/"alsa-lib"
 ALSA_LIB_URL="https://www.alsa-project.org/files/pub/lib/$ALSA_LIB.tar.bz2"
@@ -26,13 +24,17 @@ if [ ! -d $ALSA_LIB_FOLDER ]; then
   tar xf $ALSA_LIB_FILENAME -C $ALSA_LIB_FOLDER --strip-components 1
 fi
 
+cp $WORKING_DIR/config.sub $ALSA_LIB_FOLDER
 cd $ALSA_LIB_FOLDER
+export CC=aarch64-linux-gnu-gcc
+export CXX=aarch64-linux-gnu-g++
 ./configure \
   --enable-shared=no \
   --enable-static=yes \
   --with-pic \
   --host=aarch64-linux-gnu \
-  --prefix=$WORKING_DIR
+  --prefix=$WORKING_DIR \
+  --disable-python
 make clean
 make -j$NUM_CORE
 make install
